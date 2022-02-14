83572f9c036d649b9a271ebc577be9707398f5e5
kyj93790 <kyj93790@naver.com>
2022-02-14 18:51:15 +0900

    큐 2

---
+++ Queue, Deque/18258.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <queue>
+#include <deque>
+using namespace std;
+
+typedef long long ll;
+
+int main(void) {
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
+    queue<int> q;
+    ll n, x;
+    string s;
+    cin >> n;
+    while (n--) {
+        cin >> s;
+        if (!s.compare("push")) {
+            cin >> x;
+            q.push(x);
+        } else if (!s.compare("pop")) {
+            if (q.empty()) cout << -1 << '\n';
+            else {
+                cout << q.front() << '\n';
+                q.pop();
+            }
+        } else if (!s.compare("size")) {
+            cout << q.size() << '\n';
+        } else if (!s.compare("empty")) {
+            cout << q.empty() << '\n';
+        } else if (!s.compare("front")) {
+            if (q.empty()) cout << -1 << '\n';
+            else cout << q.front() << '\n';
+        } else if (!s.compare("back")) {
+            if (q.empty()) cout << -1 << '\n';
+            else cout << q.back() << '\n';
+        }
+    }
+    return 0;
+}
